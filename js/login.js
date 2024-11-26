7d4a8a6ecc8e50d1180a2e4fdf3fc864e8936d09
facuho <-->
2024-11-26 20:20:09 -0300

    Validaciones de login y register html/js

--- js/login.js
+++ js/login.js
@@ -5,15 +5,32 @@ formulario.addEventListener("submit", function (event) {
   event.preventDefault();
   let valida = true;
   if (busqueda.value == "") {
-    feedback.innerHTML = `<p class="error">El campo esta vacío</p>`;
+    feedback.innerHTML = <p class="error">El campo esta vacío</p>;
     feedback.style.display = "block";
     valida = false;
   } else if (busqueda.value.length < 3) {
-    feedback.innerHTML = `<p class="error">La búsqueda debe tener al menos 3 caracteres</p>`;
+    feedback.innerHTML = <p class="error">La búsqueda debe tener al menos 3 caracteres</p>;
     feedback.style.display = "block";
     valida = false;
   }
   if (valida) {
+    this.submit();
+  }
+});
+let login = document.querySelector("#formulario-login")
+let mail = document.querySelector("#mailf");
+let password = document.querySelector("#passwordf");
+login.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let validaf = true;
+  if (mail.value == "") {
+    alert("Por favor complete el campo email")
+    validaf = false;
+  } if (password.value == "") {
+    alert("Por favor complete el campo contraseña")
+    validaf = false;
+  }
+  if (validaf) {
     this.submit();
   }
 });
--- js/register.js
+++ js/register.js
@@ -5,15 +5,36 @@ formulario.addEventListener("submit", function (event) {
   event.preventDefault();
   let valida = true;
   if (busqueda.value == "") {
-    feedback.innerHTML = `<p class="error">El campo esta vacío</p>`;
+    feedback.innerHTML = <p class="error">El campo esta vacío</p>;
     feedback.style.display = "block";
     valida = false;
   } else if (busqueda.value.length < 3) {
-    feedback.innerHTML = `<p class="error">La búsqueda debe tener al menos 3 caracteres</p>`;
+    feedback.innerHTML = <p class="error">La búsqueda debe tener al menos 3 caracteres</p>;
     feedback.style.display = "block";
     valida = false;
   }
   if (valida) {
+    this.submit();
+  }
+});
+let register = document.querySelector("#formulario-register")
+let mail = document.querySelector("#mailr");
+let password = document.querySelector("#passwordr");
+let feedbackr1 = document.querySelector(".feedbackr1");
+let feedbackr2 = document.querySelector(".feedbackr2");
+register.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let validar = true;
+  if (mail.value == "") {
+    feedbackr1.innerHTML = <p class="error">Por favor complete el campo</p>;
+    feedbackr1.style.display = "block";
+    validar = false;
+  } if (password.value == "") {
+    feedbackr2.innerHTML = <p class="error">Por favor complete el campo</p>;
+    feedbackr2.style.display = "block";
+    validar = false;
+  }
+  if (validar) {
     this.submit();
   }
 });
