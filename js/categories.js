21afb052849d74cfaf0c54394fc55db5b14b5c3c
juanmacriniti <jcriniti@udesa.edu.ar>
2024-11-26 20:50:08 -0300

    categories html/js y css para tags

--- js/categories.js
+++ js/categories.js
@@ -5,11 +5,11 @@ formulario.addEventListener("submit", function (event) {
   event.preventDefault();
   let valida = true;
   if (busqueda.value == "") {
-    feedback.innerHTML = `<p class="error">El campo esta vacío</p>`;
+    feedback.innerHTML = <p class="error">El campo esta vacío</p>;
     feedback.style.display = "block";
     valida = false;
   } else if (busqueda.value.length < 3) {
-    feedback.innerHTML = `<p class="error">La búsqueda debe tener al menos 3 caracteres</p>`;
+    feedback.innerHTML = <p class="error">La búsqueda debe tener al menos 3 caracteres</p>;
     feedback.style.display = "block";
     valida = false;
   }
@@ -17,3 +17,22 @@ formulario.addEventListener("submit", function (event) {
     this.submit();
   }
 });
+
+fetch("https://dummyjson.com/recipes/tags")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    let section = document.querySelector(".padre-tags");
+    let character = "";
+    for (let i = 0; i < data.length; i++) {
+      character += <article class="tags">
+        <a href="./category.html?tag=${data[i]}" class="tagtext">${data[i]}</a>
+        </article>;
+      section.innerHTML = character;
+    }
+  })
+  .catch(function (error) {
+    console.log("Error: " + error);
+  });
